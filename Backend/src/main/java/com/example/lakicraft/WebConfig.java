043d5d35a43cb0923ec9f3626fbbff103b3dc448
matys <matysz21@wp.pl>
2025-07-06 20:41:54 +0200

    WebConfig

--- Backend/src/main/java/com/example/lakicraft/WebConfig.java
+++ Backend/src/main/java/com/example/lakicraft/WebConfig.java
@@ -1,4 +1,5 @@
 package com.example.lakicraft;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -8,21 +9,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
-        registry.addMapping("/**")  // Zezwolenie na dostęp do wszystkich endpointów
-                .allowedOrigins("http://localhost:3000")  // Zezwolenie tylko dla domeny frontendowej
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Dozwolone metody
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                    "https://lakicraft.netlify.app", // produkcja
+                    "http://localhost:3000"          // lokalne dev
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);  // Zezwolenie na poświadczenia
-
-                registry.addMapping("/orders/1/messages")
-                .allowedOrigins("http://localhost:3000")  // Zezwala na dostęp tylko z tej domeny
-                .allowedMethods("GET", "POST")           // Zezwala na GET i POST
-                .allowedHeaders("Authorization", "Content-Type")  // Zezwala na określone nagłówki
-                .allowCredentials(true);  // Umożliwia przesyłanie ciasteczek
+                .allowCredentials(true);
     }
- 
-    //Wyświetlanie pliku zdjęcia
+
+    // Obsługa serwowania plików z folderu uploads/
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
